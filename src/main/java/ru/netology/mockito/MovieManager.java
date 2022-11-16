06afa833cc0a8229cd4ee016419aacad7d88aaca
MaximTuktarev <maxim.tuktarev@mail.ru>
2022-11-16 18:19:25 +0400

    FirstTry

---
+++ src/main/java/ru/netology/mockito/MovieManager.java
@@ -0,0 +1,56 @@
+package ru.netology.mockito;
+
+public class MovieManager {
+    private String[] movies = new String[0];
+    private int movieLimit = 10;
+
+
+    public MovieManager(int limit) {
+        this.movieLimit = movieLimit;
+    }
+
+    public MovieManager() {
+
+    }
+
+
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+
+    public String[] findAll() {
+        return movies;
+
+    }
+
+    public String[] findLast() {
+        int resultLength = movies.length;
+        if (resultLength > movieLimit) {
+            resultLength = movieLimit;
+        } else {
+            resultLength = movies.length;
+        }
+        String[] result = new String[resultLength];
+
+        for (int i = 0; i < result.length; i++) {
+            int index = resultLength - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/ru/netology/mockito/MovieManagerTest.java
@@ -0,0 +1,76 @@
+package ru.netology.mockito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovieManagerTest {
+    @Test
+
+    public void shouldShowAll() {
+        MovieManager manager = new MovieManager();
+
+
+        manager.addMovie("Maxoynee 1");
+        manager.addMovie("Maxoynee 2");
+        manager.addMovie("Maxoynee 3");
+        manager.addMovie("Maxoynee 4");
+        manager.addMovie("Maxoynee 5");
+        manager.addMovie("Maxoynee 6");
+        manager.addMovie("Maxoynee 7");
+        manager.addMovie("Maxoynee 8");
+        manager.addMovie("Maxoynee 9");
+        manager.addMovie("Maxoynee 10");
+
+        String[] expected = {"Maxoynee 1", "Maxoynee 2", "Maxoynee 3", "Maxoynee 4", "Maxoynee 5", "Maxoynee 6", "Maxoynee 7", "Maxoynee 8", "Maxoynee 9", "Maxoynee 10"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowCustomLength() {
+        MovieManager manager = new MovieManager(5);
+
+
+        manager.addMovie("Maxoynee 1");
+        manager.addMovie("Maxoynee 2");
+        manager.addMovie("Maxoynee 3");
+        manager.addMovie("Maxoynee 4");
+        manager.addMovie("Maxoynee 5");
+
+
+        String[] expected = {"Maxoynee 5", "Maxoynee 4", "Maxoynee 3", "Maxoynee 2", "Maxoynee 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowZero() {
+        MovieManager manager = new MovieManager();
+        String[] expected = {};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowOverLimit() {
+        MovieManager manager = new MovieManager(11);
+        manager.addMovie("Maxoynee 1");
+        manager.addMovie("Maxoynee 2");
+        manager.addMovie("Maxoynee 3");
+        manager.addMovie("Maxoynee 4");
+        manager.addMovie("Maxoynee 5");
+        manager.addMovie("Maxoynee 6");
+        manager.addMovie("Maxoynee 7");
+        manager.addMovie("Maxoynee 8");
+        manager.addMovie("Maxoynee 9");
+        manager.addMovie("Maxoynee 10");
+        manager.addMovie("Maxoynee 11");
+        String[] expected = {"Maxoynee 10", "Maxoynee 9", "Maxoynee 8", "Maxoynee 7", "Maxoynee 6", "Maxoynee 5", "Maxoynee 4", "Maxoynee 3", "Maxoynee 2", "Maxoynee 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
