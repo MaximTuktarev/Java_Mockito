f43694c6abb37502848f34e829d87fee18713f43
MaximTuktarev <maxim.tuktarev@mail.ru>
2022-11-25 19:49:03 +0400

    add new tests

--- src/main/java/ru/netology/mockito/MovieManager.java
+++ src/main/java/ru/netology/mockito/MovieManager.java
@@ -4,15 +4,16 @@ public class MovieManager {
     private String[] movies = new String[0];
     private int movieLimit = 10;
 
-
-    public MovieManager(int limit) {
-        this.movieLimit = movieLimit;
-    }
-
     public MovieManager() {
 
     }
 
+    public MovieManager(int limit) {
+        if (limit > 0) {
+            movieLimit = limit;
+        }
+
+    }
 
     public void addMovie(String movie) {
         String[] tmp = new String[movies.length + 1];
@@ -37,7 +38,6 @@ public class MovieManager {
             resultLength = movies.length;
         }
         String[] result = new String[resultLength];
-
         for (int i = 0; i < result.length; i++) {
             int index = resultLength - i - 1;
             result[i] = movies[index];
--- src/test/java/ru/netology/mockito/MovieManagerTest.java
+++ src/test/java/ru/netology/mockito/MovieManagerTest.java
@@ -1,16 +1,17 @@
 package ru.netology.mockito;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 
 public class MovieManagerTest {
-    @Test
 
+
+    @Test
     public void shouldShowAll() {
         MovieManager manager = new MovieManager();
-
 
         manager.addMovie("Maxoynee 1");
         manager.addMovie("Maxoynee 2");
@@ -26,11 +27,29 @@ public class MovieManagerTest {
         String[] expected = {"Maxoynee 1", "Maxoynee 2", "Maxoynee 3", "Maxoynee 4", "Maxoynee 5", "Maxoynee 6", "Maxoynee 7", "Maxoynee 8", "Maxoynee 9", "Maxoynee 10"};
         String[] actual = manager.findAll();
 
-        Assertions.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldShowCustomLength() {
+    public void shouldShowDefaultTenFilms() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Maxoynee 1");
+        manager.addMovie("Maxoynee 2");
+        manager.addMovie("Maxoynee 3");
+        manager.addMovie("Maxoynee 4");
+        manager.addMovie("Maxoynee 5");
+        manager.addMovie("Maxoynee 6");
+        manager.addMovie("Maxoynee 7");
+        manager.addMovie("Maxoynee 8");
+        manager.addMovie("Maxoynee 9");
+        manager.addMovie("Maxoynee 10");
+        String[] expected = {"Maxoynee 10", "Maxoynee 9", "Maxoynee 8", "Maxoynee 7", "Maxoynee 6", "Maxoynee 5", "Maxoynee 4", "Maxoynee 3", "Maxoynee 2", "Maxoynee 1"};
+        String[] actual = manager.findLast();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowBelowLimit() {
         MovieManager manager = new MovieManager(5);
 
 
@@ -39,11 +58,15 @@ public class MovieManagerTest {
         manager.addMovie("Maxoynee 3");
         manager.addMovie("Maxoynee 4");
         manager.addMovie("Maxoynee 5");
-
+        manager.addMovie("Maxoynee 6");
+        manager.addMovie("Maxoynee 7");
+        manager.addMovie("Maxoynee 8");
+        manager.addMovie("Maxoynee 9");
+        manager.addMovie("Maxoynee 10");
 
         String[] expected = {"Maxoynee 5", "Maxoynee 4", "Maxoynee 3", "Maxoynee 2", "Maxoynee 1"};
         String[] actual = manager.findLast();
-        Assertions.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
@@ -51,12 +74,12 @@ public class MovieManagerTest {
         MovieManager manager = new MovieManager();
         String[] expected = {};
         String[] actual = manager.findLast();
-        Assertions.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     public void shouldShowOverLimit() {
-        MovieManager manager = new MovieManager(11);
+        MovieManager manager = new MovieManager(10);
         manager.addMovie("Maxoynee 1");
         manager.addMovie("Maxoynee 2");
         manager.addMovie("Maxoynee 3");
@@ -70,7 +93,17 @@ public class MovieManagerTest {
         manager.addMovie("Maxoynee 11");
         String[] expected = {"Maxoynee 10", "Maxoynee 9", "Maxoynee 8", "Maxoynee 7", "Maxoynee 6", "Maxoynee 5", "Maxoynee 4", "Maxoynee 3", "Maxoynee 2", "Maxoynee 1"};
         String[] actual = manager.findLast();
-        Assertions.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void shouldShowDefaultLimitWithLessMovies() {
+        MovieManager manager = new MovieManager(10);
+        manager.addMovie("Maxoynee 1");
+        manager.addMovie("Maxoynee 2");
+        manager.addMovie("Maxoynee 3");
+        String[] expected = {"Maxoynee 3", "Maxoynee 2", "Maxoynee 1"};
+        String[] actual = manager.findLast();
+        assertArrayEquals(expected, actual);
+    }
 }
